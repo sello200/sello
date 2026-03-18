17aecb23c6415db313a7fbed1aeec9a1d6901b6a
sello200 <carlossello20@gmail.com>
2026-03-19 00:34:57 +0700

    Remove comments from ello.cpp

--- ello.cpp
+++ ello.cpp
@@ -2,18 +2,15 @@
 
 int main() {
     float panjang, lebar, luas;
-
-    // Input
+    
     printf("Masukkan panjang: ");
     scanf("%f", &panjang);
 
     printf("Masukkan lebar: ");
     scanf("%f", &lebar);
 
-    // Proses
     luas = panjang * lebar;
 
-    // Output
     printf("Luas persegi panjang = %.2f\n", luas);
 
     return 0;
